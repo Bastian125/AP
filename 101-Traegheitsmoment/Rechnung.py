04c22565a0932c4cb5ab5f92bc5f21e276badf2a
Bastian Schuchardt <bastian.schuchardt@tu-dortmund.de>
2021-11-16 21:49:59 +0100

    Tolle neue Sachen

--- 101-Traegheitsmoment/Rechnung.py
+++ 101-Traegheitsmoment/Rechnung.py
@@ -1,9 +1,17 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+# D durch mitteln bestimmen
 F, phi, r = np.genfromtxt('Messdaten_D.txt', unpack=True)
 
 def D(F, phi, r):
     return (F*r)/(phi)
 
 D = D(F, phi, r)
-print(np.mean(D))
+#print(np.mean(D))
+
+# I_d durch Lineare Regression bestimmen
+T, a = np.genfromtxt('Messdaten_I_d.txt', unpack=True)
+
+plt.plot(T, a)
+plt.savefig('test.pdf')
