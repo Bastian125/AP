73681e4bb9ac51c6169ea19490bc946c33975a6e
Bastian Schuchardt <bastian.schuchardt@tu-dortmund.de>
2021-11-17 14:42:48 +0100

    Auswertungen...

--- 101-Traegheitsmoment/Rechnung.py
+++ 101-Traegheitsmoment/Rechnung.py
@@ -12,5 +12,11 @@ D_r = np.round(D, 6)
 print('D: ', np.round(D_r, 6),'\n Mittelwert: ', np.round(np.mean(D_r), 6),'\n Abweichung: ', np.round(np.std(D_r), 6))
 
 # T_Zyl durch mitteln bestimmen
-T_Zyl = np.genfromtxt('Messdaten_Zylinder.txt', unpack=True)
+Z_Zyl = np.genfromtxt('Messdaten_Zylinder.txt', unpack=True)
+T_Zyl = np.round(Z_Zyl/5, 2)
 print('\n T_Zyl: ', T_Zyl, '\n Mittelwert: ', np.round(np.mean(T_Zyl), 2), '\n Abweichung: ', np.round(np.std(T_Zyl), 2))
+
+# T_Kugel durch mitteln bestimmen
+Z_Kugel = np.genfromtxt('Messdaten_Kugel.txt', unpack=True)
+T_Kugel = np.round(Z_Kugel/3, 2)
+print('\n T_Kugel: ', T_Kugel, '\n Mittelwert: ', np.round(np.mean(T_Kugel), 2), '\n Abweichung: ', np.round(np.std(T_Kugel), 2))
