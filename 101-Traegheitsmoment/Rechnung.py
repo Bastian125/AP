05145a3f16737f30a82f3a4b3f7ff505309a01a8
Bastian Schuchardt <bastian.schuchardt@tu-dortmund.de>
2021-11-19 15:35:33 +0100

    Zwischenstand Freitag

--- 101-Traegheitsmoment/Rechnung.py
+++ 101-Traegheitsmoment/Rechnung.py
@@ -33,6 +33,22 @@ T_K2 = np.round(Z_K2/3, 2)
 print('\n T_K2: ', T_K2, '\n Mittelwert: ', np.round(np.mean(T_K2), 2), '\n Abweichung: ', np.round(np.std(T_K2), 2))
 
 # Maße des Koerpers
+# Maße Kopf
 r_K = np.genfromtxt('r_kopf.txt', unpack=True)
 r_k = np.round(r_K, 4)
 print('\n r_k: ', r_k, '\n Mittelwert: ', np.round(np.mean(r_k), 4), '\n Abweichung: ', np.round(np.std(r_k), 4))
+
+# Maße Oberkoerper
+r_O = np.genfromtxt('r_ober.txt', unpack=True)
+r_o = np.round(r_O, 4)
+print('\n r_o: ', r_o, '\n Mittelwert: ', np.round(np.mean(r_o), 4), '\n Abweichung: ', np.round(np.std(r_o), 4))
+
+# Maße Arme
+r_A = np.genfromtxt('r_arme.txt', unpack=True)
+r_a = np.round(r_A, 4)
+print('\n r_a: ', r_a, '\n Mittelwert: ', np.round(np.mean(r_a), 4), '\n Abweichung: ', np.round(np.std(r_a), 4))
+
+# Maße Beine
+r_B = np.genfromtxt('r_beine.txt', unpack=True)
+r_b = np.round(r_B, 4)
+print('\n r_b: ', r_b, '\n Mittelwert: ', np.round(np.mean(r_b), 4), '\n Abweichung: ', np.round(np.std(r_b), 4))
