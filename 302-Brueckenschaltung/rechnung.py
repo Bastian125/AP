43a77efab7b482217ef1fb909ced70061ae2d6ad
Bastian Schuchardt <bastian.schuchardt@tu-dortmund.de>
2021-12-01 18:31:52 +0100

    Yeah Tabellen

--- 302-Brueckenschaltung/rechnung.py
+++ 302-Brueckenschaltung/rechnung.py
@@ -1,12 +1,23 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 
+# a)
 # R14 bestimmen
 R2, R3, R4, Rx = np.genfromtxt('Ma1).txt', unpack=True)
 R14 = R2*R3/R4
-print('R14: ', R14)
+print('R14: ', np.mean(R14))
 
 # R13 bestimmen
-R2, R3, R4, Rx = np.genfromtxt('Ma2).txt', unpack=True)
+R2, R3, R4, Rt = np.genfromtxt('Ma2).txt', unpack=True)
 R13 = R2*R3/R4
-print('R13: ', R13)
+print('R13: ', np.mean(R13))
+
+# b)
+# Cx und Rx
+C2 = 597*10**(-9)
+R2, R3, R4, Ct, Rt1 = np.genfromtxt('Mb).txt', unpack=True)
+Cx = C2*R4/R3
+Rx = R2*R3/R4
+print('Cx: ', np.mean(Cx))
+print('Rx: ', np.mean(Rx))
+
