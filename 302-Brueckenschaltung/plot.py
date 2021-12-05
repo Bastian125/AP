3c9d34fb9aabf9a4209d280e584783b4544c471d
Bastian Schuchardt <bastian.schuchardt@tu-dortmund.de>
2021-12-05 15:53:02 +0100

    Plot fertig

--- 302-Brueckenschaltung/plot.py
+++ 302-Brueckenschaltung/plot.py
@@ -6,6 +6,7 @@ x = f/241
 y = U_br/U_s
 
 plt.plot(x, y, 'x', label='Messwerte')
+plt.plot(x, (1)/(9) * ((x**2 -1)**2)/(((1-x**2)**2)+9*x**2), label='Theoriekurve')
 plt.xlabel(r'$\Omega = \frac{f}{f_0}$')
 plt.xscale('log')
 plt.ylabel(r'$\frac{U_{Br}}{U_s}$')
