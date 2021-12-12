4d702d8049b327e61c907fbddcc8bf138eb553a9
Tabea Hacheney <tabea.hacheney@tu-dortmund.de>
2021-12-12 20:13:05 +0100

    Auswertung fertig :)

--- V353-RelaxationsverhaltenRCKreis/plot.py
+++ V353-RelaxationsverhaltenRCKreis/plot.py
@@ -71,8 +71,8 @@ print(f'RC = {10**6*T:.1f} ± {10**6*err:.2} micro-seconds')
 
  #plot
 f_plt = np.linspace(3*10**2,10**5,10**4)
-plt.scatter(f,A, marker='+', label='Messwerte')
-plt.plot(f_plt, AC(f_plt, T), color='r', label=rf'Ausgleichsfunktion',
+plt.scatter(f,A, marker='+', color='gold', label='Messwerte')
+plt.plot(f_plt, AC(f_plt, T), color='navy', label=rf'Ausgleichsfunktion',
         linewidth=0.4)
 
 plt.title(r'Spannungsverhältnis $A(\omega) / U_0$ im RC-Kreis')
@@ -83,3 +83,133 @@ plt.legend()
 
 plt.savefig('build/plot2.pdf')
 #plt.show()
+plt.clf()
+
+### 4c ###
+
+# daten input, anpassung auf SI-Größen
+f, A, U, a, b = np.genfromtxt('Mbc.txt', unpack=True)
+
+# Spannung der Quelle
+U0 = 1.6
+
+# merge arrays
+phi = a/b * 2 * np.pi
+
+# neues dataframe fuer ausgabe
+d = {'f': f, 'a': a, 'b':b, 'phi':phi}
+df_new = pd.DataFrame(d)
+print(df_new)
+
+# curve fit
+def phi_fit(f, T):
+    omega = 2 * np.pi * f
+    return np.arctan(-1 * omega * T)
+
+p0 = [0.00089]
+T, pcov = curve_fit(phi_fit, f, phi, p0=p0)
+err = np.sqrt(np.diag(pcov))[0]
+T = T[0]
+
+# output
+print(f'RC = ({-1*T * 10**6:.4f} ± {err * 10*6:.4f}) micro-seconds')
+
+# plot
+phi_plt = np.linspace(30, 10**5, 10**4)
+plt.plot(phi_plt, phi_fit(phi_plt, T), color='navy', label=rf'Ausgleichsfunktion')
+plt.scatter(f,phi, marker='+', color='gold', label='Messwerte')
+
+plt.title(r'Phasenverschiebung zwischen $U_C$ und $U_G$')
+plt.xlabel(r'$f / \unit{\hertz}$')
+plt.ylabel(r'$\varphi /$ rad')
+plt.legend()
+#plt.xlim(0,10000)
+plt.xscale('log')
+
+plt.savefig('build/plot3.pdf')
+plt.clf()
+
+# phasenverschiebung.py
+# daten input, anpassung auf SI-Größen
+
+f, A, U, a, b = np.genfromtxt('Mbc.txt', unpack=True)
+# Spannung der Quelle
+U0 = 1.6
+
+phi = a/b * 2 * np.pi
+
+f_c = f
+phi_c = phi
+
+# curve fit
+def phi_fit(f, T):
+    omega = 2 * np.pi * f
+    return np.arctan(-1 * omega * T)
+
+p0 = [0.00089]
+Tc, pcov = curve_fit(phi_fit, f, phi, p0=p0)
+err = np.sqrt(np.diag(pcov))[0]
+Tc = -1 * Tc[0] # RC nach berechnung von 4c
+
+# output
+T = Tc
+print('Fit-Ergebnisse für Phasenverschiebung:')
+print(f'RC = ({T * 10**6:.4f} ± {err * 10*6:.4f}) micro-seconds')
+
+# amplitudenverhaeltnis
+# daten input, anpassung auf SI-Größen
+
+# Spannung der Quelle
+U0 = 1.6
+
+# anpassung auf spannungsverhaeltnis
+A = A / 1.6
+
+# curve fit
+def AC(f, T):
+    U0 = 1 # damit das Verhaeltnis dargestellt wird
+    w = 2 * np.pi * f
+    return 2.44 / (np.sqrt(1 + w**2 * T**2))
+
+p0 = [0.0008]
+om = 2 * np.pi * f
+popt, pcov = curve_fit(AC, f, A, p0=p0)
+U0 = 1.6
+Tb = popt[0]
+err = np.sqrt(np.diag(pcov))[0]
+
+# output
+T = Tb
+print('Fit-Ergebnisse für Amplitudenverhaeltnis:')
+print(f'RC = {10**6*T:.1f} ± {10**6*err:.2} micro-seconds')
+
+# plotting
+fig = plt.figure()
+ax = fig.add_subplot(111, polar=True)
+
+RC = Tc
+# plot von messwerten
+ax.scatter(phi_c, AC(f_c, RC), marker='+', 
+        linewidth=1, color='r', label='Messwerte der Phasenverschiebung')
+ax.scatter(phi_fit(-1 * f[0:7], RC), A[0:7], marker='x', color='navy',
+        linewidth=0.8, label='Messwerte des Amplitudenverhältnis')
+
+f = np.linspace(10**-2, 10**4, 10**5)
+# plot fuer wert laut phase
+plt.polar(phi_fit(-1 * f, RC), AC(f, RC), label=rf'Ausgleichsfunktion für die Zeitkonstante aus 4c',
+        linewidth=0.5, color='gold')
+
+ax.set_thetamin(120)
+ax.set_thetamax(-10)
+#ax.set_rlabel_position(0)
+#ax.legend(loc=l, bbox_to_anchor=(0.6,0.5))
+ax.grid(True)
+
+plt.title('Polarplot')
+plt.xticks([0, np.pi/8, np.pi/4,  3*np.pi/8, np.pi/2],
+        [r"$0$", r"$\frac{\pi}{8}$", r"$\frac{\pi}{4}$",  r"$\frac{3\pi}{8}$", r"$\frac{\pi}{2}$"])
+plt.legend()
+plt.tight_layout()
+plt.savefig('build/polarplot.pdf')
+
+#""" polarplot.py end """
