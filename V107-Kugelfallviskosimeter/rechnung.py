a2e4dbbfcba9889c939be4f6c477e531f773b1a8
Bastian Schuchardt <bastian.schuchardt@tu-dortmund.de>
2021-12-17 12:16:54 +0100

    Keine Einrueckungen

--- V107-Kugelfallviskosimeter/rechnung.py
+++ V107-Kugelfallviskosimeter/rechnung.py
@@ -22,3 +22,14 @@ print("Kleine Kugel:")
 print("r: ", np.round(np.mean(r_kl), 5), " +- ", np.round(np.std(r_kl), 5))
 print("m: ", np.round(np.mean(m_kl), 5), " +- ", np.round(np.std(m_kl), 5))
 print("d: ", d_K)
+
+# Fallzeiten
+# Große Kugel
+T_gr = np.genfromtxt('FallzeitGr.txt', unpack=True)
+print("Große Kugel: ")
+print("t: ", np.round(np.mean(T_gr), 2), " +- ", np.round(np.std(T_gr), 2))
+
+# Kleine Kugel
+T_kl = np.genfromtxt('FallzeitKl.txt', unpack=True)
+print("Kleine Kugel: ")
+print("t: ", np.round(np.mean(T_kl), 2), " +- ", np.round(np.std(T_kl), 2))
