4022b04f92a2c7161b2ace2b42414f7464f37a88
Bastian Schuchardt <bastian.schuchardt@tu-dortmund.de>
2022-01-14 14:18:16 +0100

    Letzter Stand Freitag

--- V204-Waermeleitfaehigkeit/rechnung.py
+++ V204-Waermeleitfaehigkeit/rechnung.py
@@ -3,3 +3,7 @@ import numpy as np
 x = np.genfromtxt('messdaten/x.txt', unpack=True)
 
 print('x: ', np.mean(x), ' +- ', np.std(x))
+
+t, T7, T2, T1, T3, T4, T5, T6, T8 = np.genfromtxt('messdaten/statischeMethode.txt', encoding="UTF-16", unpack=True)
+
+print(T7[95]-T8[95])
