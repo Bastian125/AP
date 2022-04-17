aa4d1802a0c8a575b0e41c5ef02ac6ad5418b835
Bastian Schuchardt <bastian.schuchardt@tu-dortmund.de>
2022-04-17 23:23:02 +0200

    Zwischenstand...

--- V500-Photoeffekt/vXXX/plot.py
+++ V500-Photoeffekt/vXXX/plot.py
@@ -28,3 +28,91 @@ plt.ylabel(r'$\frac{I}{A}$')
 plt.grid()
 plt.legend()
 plt.savefig('build/plot1.pdf')
+plt.close()
+
+# Gelbe Spektrallinie
+# Daten laden
+U, I = np.genfromtxt('a2.txt' , unpack=True)
+
+# Lineare Regression
+params, covariance_matrix = np.polyfit(U, I, deg=1, cov=True)
+
+errors = np.sqrt(np.diag(covariance_matrix))
+
+for name, value, error in zip('ab', params, errors):
+    print(f'{name} = {value} ± {error}')
+
+# Plot
+x_plot = np.linspace(0, 0.45)
+plt.plot(U, I, 'x', label="Messwerte")
+plt.plot(
+    x_plot,
+    params[0] * x_plot + params[1],
+    label='Lineare Regression',
+    linewidth=3,
+)
+# Label
+plt.xlabel(r'$\frac{U_B}{V}$')
+plt.ylabel(r'$\frac{I}{A}$')
+plt.grid()
+plt.legend()
+plt.savefig('build/plot2.pdf')
+plt.close()
+
+# Gruene Spektrallinie
+# Daten laden
+U, I = np.genfromtxt('a3.txt' , unpack=True)
+
+# Lineare Regression
+params, covariance_matrix = np.polyfit(U, I, deg=1, cov=True)
+
+errors = np.sqrt(np.diag(covariance_matrix))
+
+for name, value, error in zip('ab', params, errors):
+    print(f'{name} = {value} ± {error}')
+
+# Plot
+x_plot = np.linspace(0, 0.5)
+plt.plot(U, I, 'x', label="Messwerte")
+plt.plot(
+    x_plot,
+    params[0] * x_plot + params[1],
+    label='Lineare Regression',
+    linewidth=3,
+)
+# Label
+plt.xlabel(r'$\frac{U_B}{V}$')
+plt.ylabel(r'$\frac{I}{A}$')
+plt.grid()
+plt.legend()
+plt.savefig('build/plot3.pdf')
+plt.close()
+
+# Gelbe Spektrallinie
+# Daten laden
+U, I = np.genfromtxt('a4.txt' , unpack=True)
+
+# Lineare Regression
+params, covariance_matrix = np.polyfit(U, I, deg=1, cov=True)
+
+errors = np.sqrt(np.diag(covariance_matrix))
+
+for name, value, error in zip('ab', params, errors):
+    print(f'{name} = {value} ± {error}')
+
+# Plot
+x_plot = np.linspace(0, 0.9)
+plt.plot(U, I, 'x', label="Messwerte")
+plt.plot(
+    x_plot,
+    params[0] * x_plot + params[1],
+    label='Lineare Regression',
+    linewidth=3,
+)
+# Label
+plt.xlabel(r'$\frac{U_B}{V}$')
+plt.ylabel(r'$\frac{I}{A}$')
+plt.grid()
+plt.legend()
+plt.savefig('build/plot4.pdf')
+plt.close()
