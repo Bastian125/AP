105df6da2f74d609bd0cebbc649dc905845b9009
Bastian Schuchardt <bastian.schuchardt@tu-dortmund.de>
2022-04-18 14:57:29 +0200

    Zwischenstand

--- V500-Photoeffekt/vXXX/plot.py
+++ V500-Photoeffekt/vXXX/plot.py
@@ -120,3 +120,59 @@ plt.grid()
 plt.legend()
 plt.savefig('build/plot4.pdf')
 plt.close()
+
+# Grenzspannung bestimmen
+from uncertainties import ufloat
+a1 = ufloat(-1.47, 0.12)
+a2 = ufloat(-7.06, 0.15)
+a3 = ufloat(-14.81, 0.26)
+a4 = ufloat(-7.68, 0.14)
+
+b1 = ufloat(0.59, 0.02)
+b2 = ufloat(3.05, 0.04)
+b3 = ufloat(7.26, 0.07)
+b4 = ufloat(7.05, 0.07)
+
+print("U1: ", -b1/a1)
+print("U2: ", -b2/a2)
+print("U3: ", -b3/a3)
+print("U4: ", -b4/a4)
+
+# Grenzspannung gegen Frequenz auftragen
+f  = [4.64e14, 5.19e14, 5.49e14, 6.89e14]
+UG = [0.401, 0.432, 0.490, 0.918]
+
+# Lineare Regression
+params, covariance_matrix = np.polyfit(f, UG, deg=1, cov=True)
+
+errors = np.sqrt(np.diag(covariance_matrix))
+
+for name, value, error in zip('ab', params, errors):
+    print(f'{name} = {value} ± {error}')
+
+# Plot
+x_plot = np.linspace(0, 8e14)
+plt.plot(f, UG, 'x', label=r'$U_{G}/V$')
+plt.plot(
+    x_plot,
+    params[0] * x_plot + params[1],
+    label='Lineare Regression',
+    linewidth=3,
+)
+plt.xlabel(r'$\frac{f}{Hz}$')
+plt.ylabel(r'$\frac{U_G}{V}$')
+plt.grid()
+plt.legend()
+plt.savefig('build/plot5.pdf')
+plt.close()
+
+# Spannung ueber den ganzen Bereich
+U, I = np.genfromtxt('b.txt', unpack=True)
+
+plt.plot(U, I, 'x', label="Messdaten der gelben Spektrallinie.")
+plt.grid()
+plt.xlabel('U/V')
+plt.ylabel('I/A')
+plt.legend()
+plt.savefig('build/plot6.pdf')
+plt.close()
