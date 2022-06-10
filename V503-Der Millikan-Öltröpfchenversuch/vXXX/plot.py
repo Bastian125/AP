175edd0bffae9f0525d3a9a57c0bed319ddfb283
Bastian Schuchardt <bastian.schuchardt@tu-dortmund.de>
2022-06-10 16:51:43 +0200

    Zwischenstand

--- V503-Der Millikan-Öltröpfchenversuch/vXXX/plot.py
+++ V503-Der Millikan-Öltröpfchenversuch/vXXX/plot.py
@@ -19,7 +19,7 @@ e = const.e
 # Geschwindigkeit v
 def v(t):
     t = ufloat(np.mean(t), np.std(t))
-    return (s/t)*10**(2)
+    return (s/t)
 
 # Feldstärke des Plattenkondensators
 def E(U):
@@ -27,15 +27,24 @@ def E(U):
 
 # Radius des Öltröpfchens
 def r(vab, vauf):
-    return np.sqrt((9/2)*(nL/g)*(vab-vauf)/(dO-dL))
+    if vab-vauf<0:
+        return 0
+    else:
+        return ((9/2)*(nL/g)*(vab-vauf)/(dO-dL))**(0.5)
 
 # Unkorriegierte Ladung
 def q(vab, vauf, E, r):
-    return (3/4)*np.pi*nL*r*((vab+vauf)/(E))
+    if r==0:
+        return 0
+    else:
+        return (3/4)*np.pi*nL*r*((vab+vauf)/(E))
 
 # Korrigierte Ladung
 def qkorr(q, r):
-    return q*(1+(B/p*r))
+    if q==0:
+        return 0
+    else:
+        return q*(1+(B/p*r))
 
 # Geschwindikeiten
 # Daten einlesen
@@ -164,64 +173,134 @@ E4 = E(210)
 E5 = E(240)
 
 # Radius
+r1  = r(v111, v112)
+r2  = r(v121, v122)
+r3  = r(v131, v132)
+r4  = r(v141, v142)
+r5  = r(v151, v152)
+r6  = r(v211, v212)
+r7  = r(v221, v222)
+r8  = r(v231, v232)
+r9  = r(v241, v242)
+r10 = r(v251, v252)
+r11 = r(v311, v312)
+r12 = r(v321, v322)
+r13 = r(v331, v332)
+r14 = r(v341, v342)
+r15 = r(v351, v352)
+r16 = r(v411, v412)
+r17 = r(v421, v422)
+r18 = r(v431, v432)
+r19 = r(v441, v442)
+r20 = r(v451, v452)
+r21 = r(v511, v512)
+r22 = r(v521, v522)
+r23 = r(v531, v532)
+r24 = r(v541, v542)
+r25 = r(v551, v552)
+
+# Ladung
+q1  = q(v111, v112, E1, r1)
+q2  = q(v121, v122, E1, r2)
+q3  = q(v131, v132, E1, r3)
+q4  = q(v141, v142, E1, r4)
+q5  = q(v151, v152, E1, r5)
+q6  = q(v211, v212, E2, r6)
+q7  = q(v221, v222, E2, r7)
+q8  = q(v231, v232, E2, r8)
+q9  = q(v241, v242, E2, r9)
+q10 = q(v251, v252, E2, r10)
+
 
 # Ausgabe
 
-print('v111: ', v111, ', v112: ', v112)
-print('v121: ', v121, ', v122: ', v122)
-print('v131: ', v131, ', v132: ', v132)
-print('v141: ', v141, ', v142: ', v142)
-print('v151: ', v151, ', v152: ', v152)
+#print('v111: ', v111, ', v112: ', v112)
+#print('v121: ', v121, ', v122: ', v122)
+#print('v131: ', v131, ', v132: ', v132)
+#print('v141: ', v141, ', v142: ', v142)
+#print('v151: ', v151, ', v152: ', v152)
+#
+#print('v211: ', v211, ', v212: ', v212)
+#print('v221: ', v221, ', v222: ', v222)
+#print('v231: ', v231, ', v232: ', v232)
+#print('v241: ', v241, ', v242: ', v242)
+#print('v251: ', v251, ', v252: ', v252)
+#
+#print('v311: ', v311, ', v312: ', v312)
+#print('v321: ', v321, ', v322: ', v322)
+#print('v331: ', v331, ', v332: ', v332)
+#print('v341: ', v341, ', v342: ', v342)
+#print('v351: ', v351, ', v352: ', v352)
+#
+#print('v411: ', v411, ', v412: ', v412)
+#print('v421: ', v421, ', v422: ', v422)
+#print('v431: ', v431, ', v432: ', v432)
+#print('v441: ', v441, ', v442: ', v442)
+#print('v451: ', v451, ', v452: ', v452)
+#
+#print('v511: ', v511, ', v512: ', v512)
+#print('v521: ', v521, ', v522: ', v522)
+#print('v531: ', v531, ', v532: ', v532)
+#print('v541: ', v541, ', v542: ', v542)
+#print('v551: ', v551, ', v552: ', v552)
+#
+#print('v1 : ', v1)
+#print('v2 : ', v2)
+#print('v3 : ', v3)
+#print('v4 : ', v4)
+#print('v5 : ', v5)
+#print('v6 : ', v6)
+#print('v7 : ', v7)
+#print('v8 : ', v8)
+#print('v9 : ', v9)
+#print('v10: ', v10)
+#print('v11: ', v11)
+#print('v12: ', v12)
+#print('v13: ', v13)
+#print('v14: ', v14)
+#print('v15: ', v15)
+#print('v16: ', v16)
+#print('v17: ', v17)
+#print('v18: ', v18)
+#print('v19: ', v19)
+#print('v20: ', v20)
+#print('v21: ', v21)
+#print('v22: ', v22)
+#print('v23: ', v23)
+#print('v24: ', v24)
+#print('v25: ', v25)
 
-print('v211: ', v211, ', v212: ', v212)
-print('v221: ', v221, ', v222: ', v222)
-print('v231: ', v231, ', v232: ', v232)
-print('v241: ', v241, ', v242: ', v242)
-print('v251: ', v251, ', v252: ', v252)
+#print('r1  : ',  r1)
+#print('r2  : ',  r2)
+#print('r3  : ',  r3)
+#print('r4  : ',  r4)
+#print('r5  : ',  r5)
+#print('r6  : ',  r6)
+#print('r7  : ',  r7)
+#print('r8  : ',  r8)
+#print('r9  : ',  r9)
+#print('r10 : ', r10)
+#print('r11 : ', r11)
+#print('r12 : ', r12)
+#print('r13 : ', r13)
+#print('r14 : ', r14)
+#print('r15 : ', r15)
+#print('r16 : ', r16)
+#print('r17 : ', r17)
+#print('r18 : ', r18)
+#print('r19 : ', r19)
+#print('r20 : ', r20)
+#print('r21 : ', r21)
+#print('r22 : ', r22)
+#print('r23 : ', r23)
+#print('r24 : ', r24)
+#print('r25 : ', r25)
 
-print('v311: ', v311, ', v312: ', v312)
-print('v321: ', v321, ', v322: ', v322)
-print('v331: ', v331, ', v332: ', v332)
-print('v341: ', v341, ', v342: ', v342)
-print('v351: ', v351, ', v352: ', v352)
-
-print('v411: ', v411, ', v412: ', v412)
-print('v421: ', v421, ', v422: ', v422)
-print('v431: ', v431, ', v432: ', v432)
-print('v441: ', v441, ', v442: ', v442)
-print('v451: ', v451, ', v452: ', v452)
-
-print('v511: ', v511, ', v512: ', v512)
-print('v521: ', v521, ', v522: ', v522)
-print('v531: ', v531, ', v532: ', v532)
-print('v541: ', v541, ', v542: ', v542)
-print('v551: ', v551, ', v552: ', v552)
-
-print('v1 : ', v1)
-print('v2 : ', v2)
-print('v3 : ', v3)
-print('v4 : ', v4)
-print('v5 : ', v5)
-print('v6 : ', v6)
-print('v7 : ', v7)
-print('v8 : ', v8)
-print('v9 : ', v9)
-print('v10: ', v10)
-print('v11: ', v11)
-print('v12: ', v12)
-print('v13: ', v13)
-print('v14: ', v14)
-print('v15: ', v15)
-print('v16: ', v16)
-print('v17: ', v17)
-print('v18: ', v18)
-print('v19: ', v19)
-print('v20: ', v20)
-print('v21: ', v21)
-print('v22: ', v22)
-print('v23: ', v23)
-print('v24: ', v24)
-print('v25: ', v25)
+print('q1 : ', q1)
+print('q2 : ', q2)
+print('q3 : ', q3)
+print('q4 : ', q4)
+print('q5 : ', q5)
 
 x=1
 y=2
